896c31bbc38f5283ca75a8af86f87979de475f89
Jack Young <jackp@vayoungs.us>
2014-10-30 12:27:45 -0400

    Admin nag works

--- application/views/backend/common/nav.php
+++ application/views/backend/common/nav.php
@@ -4,16 +4,16 @@
 
             <ul class="nav-list list dn db--m">
                 <li class="is-active list-item nav-item">
-                    <a href="adminEditIndex.html" class="nav-item-link list-item-link">Edit Home Page</a>
+                    <a href="<?php echo base_url(); ?>index.php/admin/goto_edit_home_page" class="nav-item-link list-item-link">Edit Home Page</a>
                 </li>
                 <li class="nav-item list-item">
-                    <a href="adminAddUser.html" class="nav-item-link list-item-link">Add Speaker</a>
+                    <a href="<?php echo base_url(); ?>index.php/admin/goto_add_speaker_page" class="nav-item-link list-item-link">Add Speaker</a>
                 </li>
                 <li class="nav-item list-item">
-                    <a href="adminDeleteSpeaker.html" class="nav-item-link list-item-link">Delete Speakers</a>
+                    <a href="<?php echo base_url(); ?>index.php/admin/goto_delete_speakers_page" class="nav-item-link list-item-link">Delete Speakers</a>
                 </li>
                 <li class="nav-item list-item">
-                    <a href="adminTalksEdits.html" class="nav-item-link list-item-link">Delete Lectures</a>
+                    <a href="<?php echo base_url(); ?>index.php/admin/goto_delete_lectures_page" class="nav-item-link list-item-link">Delete Lectures</a>
                 </li>
             </ul>
         </nav>
